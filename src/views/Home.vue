744925bfc6cd5428914a0fc94789aa3cfc810462
wanglifa <wanglifa1995@gmail.com>
2020-08-11 16:17:18 +0800

    topnav样式更新

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,15 @@
 <template>
   <div>
     <div class="topnav">
-      <div class="logo"></div>
-      <div class="menu"></div>
+      <div class="logo">LOGO</div>
+      <ul class="menu">
+        <li>菜单1</li>
+        <li>菜单2</li>
+      </ul>
     </div>
     <div class="banner">
-      <h1>IVue</h1>
-      <h2>一个厉害的UI框架</h2>
+      <h1>轱辘UI</h1>
+      <h2>一个厉害的 UI 框架</h2>
       <p class="actions">
         <a>GitHub</a>
         <a>开始</a>
@@ -15,5 +18,21 @@
   </div>
 </template>
 <style lang="scss" scoped>
-  .topnav {border: 1px solid red}
+  .topnav {
+    background: pink;
+    display: flex;
+    padding: 16px;
+    > .logo {
+      max-width: 6em;
+      margin-right: auto;
+    }
+    > .menu {
+      display: flex;
+      white-space: nowrap;
+      flex-wrap: nowrap;
+      > li {
+        margin: 0 1em;
+      }
+    }
+  }
 </style>
