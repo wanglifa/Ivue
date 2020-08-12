28fd9f5b3c8ffc84229c8fc21f9ea22e03b434b1
wanglifa <wanglifa1995@gmail.com>
2020-08-12 15:11:18 +0800

    Doc html初始化

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,11 +2,11 @@
   <div>
     <top-nav></top-nav>
     <div class="banner">
-      <h1>轱辘UI</h1>
+      <h1>IVue</h1>
       <h2>一个厉害的 UI 框架</h2>
       <p class="actions">
-        <a>GitHub</a>
-        <a>开始</a>
+        <a href="https://github.com">GitHub</a>
+        <router-link>开始</router-link>
       </p>
     </div>
   </div>
@@ -19,5 +19,25 @@
   }
 </script>
 <style lang="scss" scoped>
-
+  .banner {
+    padding: 100px 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    background: lightgreen;
+    > .actions{
+      padding: 8px 0;
+      a {
+        margin:0 8px;
+        background: #fff;
+        display: inline-block;
+        $h: 28px;
+        height: $h;
+        line-height: $h;
+        border-radius: $h/2;
+        padding: 0 8px;
+      }
+    }
+  }
 </style>
