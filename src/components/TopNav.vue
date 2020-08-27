b71e88a775d8981c947bfe90b7b137cc2dd91bb2
wanglifa <wanglifa1995@gmail.com>
2020-08-27 16:08:44 +0800

    添加topAside

--- src/components/TopNav.vue
+++ src/components/TopNav.vue
@@ -5,6 +5,7 @@
       <li>菜单1</li>
       <li>菜单2</li>
     </ul>
+    <span class="toggleAside"></span>
   </div>
 </template>
 <script lang="ts">
@@ -25,6 +26,10 @@
     background: pink;
     display: flex;
     padding: 16px;
+    position: relative;
+    z-index: 10;
+    justify-content: center;
+    align-items: center;
     > .logo {
       max-width: 6em;
       margin-right: auto;
@@ -36,6 +41,20 @@
       > li {
         margin: 0 1em;
       }
+    }
+    > .toggleAside {
+      display: inline-block;
+      width: 24px;
+      height: 24px;
+      background: red;
+      position: absolute;
+      left: 16px;
+      top: 50%;
+      transform: translateY(-50%);
+    }
+    @media (max-width:500px) {
+      > .menu{display: none;}
+      > .logo{margin: 0 auto;}
     }
   }
 </style>
