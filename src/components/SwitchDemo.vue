51f55f41a4b9a57e7c21f5c4176c24c4aed4483b
wanglifa <wanglifa1995@gmail.com>
2020-09-07 14:40:13 +0800

    可以接受value和input事件

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -1,12 +1,17 @@
 <template>
   <div>
-    <Switch/>
+    <Switch :value="x" @input="x = $event"/>
   </div>
 </template>
 <script lang="ts">
   import Switch from '../lib/Switch.vue'
+  import { ref } from 'vue'
   export default {
     name: 'SwitchDemo',
+    setup() {
+      const x = ref(true)
+      return { x }
+    },
     components: { Switch }
   }
 </script>
