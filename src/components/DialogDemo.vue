f9f2ad546bb42ad6cd37a81270c30b021cc72bf1
wanglifa <wanglifa1995@gmail.com>
2020-09-15 15:07:34 +0800

    创建Dialog

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,11 +1,13 @@
 <template>
   <div>
-    Dialog示例
+    <Dialog></Dialog>
   </div>
 </template>
 <script lang="ts">
+  import Dialog from '../lib/Dialog.vue'
   export default {
     name: 'Switch',
+    components: {Dialog},
     setup() {
 
     }
