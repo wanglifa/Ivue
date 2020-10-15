9a0b2b4450c1c65d0f1154742e2fdf3c56120a81
wanglifa <wanglifa1995@gmail.com>
2020-10-15 11:01:12 +0800

    引入github-markdown-css

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import './lib/ivue3.scss'
 import './index.scss'
 import { createApp } from 'vue'
 import App from './App.vue'
+import 'github-markdown-css'
 import {router} from './router'
 const app = createApp(App)
 app.use(router)
