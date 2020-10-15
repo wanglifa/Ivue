e4b82f5073ab6b5c4a9f0d8854a34617b487c9e8
wanglifa <wanglifa1995@gmail.com>
2020-10-15 10:38:03 +0800

    导航菜单选中高亮

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -32,69 +32,78 @@
         </ol>
       </aside>
       <main>
-        <router-view />
+        <router-view/>
       </main>
     </div>
   </div>
 </template>
 <script lang="ts">
-  import Topnav from "../components/Topnav.vue";
-  import { inject, Ref } from "vue";
-  export default {
-    components: { Topnav },
-    setup() {
-      const menuVisible = inject<Ref<boolean>>("menuVisible"); // get
-      return { menuVisible };
-    },
-  };
+import Topnav from '../components/TopNav.vue';
+import {inject, Ref} from 'vue';
+
+export default {
+  components: {Topnav},
+  setup() {
+    const menuVisible = inject<Ref<boolean>>('menuVisible'); // get
+    return {menuVisible};
+  },
+};
 </script>
 <style lang="scss" scoped>
-  .layout {
-    display: flex;
-    flex-direction: column;
-    height: 100vh;
-    > .nav {
-      flex-shrink: 0;
-    }
-    > .content {
-      flex-grow: 1;
-      padding-top: 60px;
-      padding-left: 156px;
-      @media (max-width: 500px) {
-        padding-left: 0;
-      }
+.layout {
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  >.nav {
+    flex-shrink: 0;
+  }
+  >.content {
+    flex-grow: 1;
+    padding-top: 60px;
+    padding-left: 156px;
+    @media (max-width: 500px) {
+      padding-left: 0;
     }
   }
-  .content {
-    display: flex;
-    > aside {
-      flex-shrink: 0;
-    }
-    > main {
-      flex-grow: 1;
-      padding: 16px;
-      background: white;
-    }
+}
+.content {
+  display: flex;
+  >aside {
+    flex-shrink: 0;
   }
-  aside {
-    background: lightblue;
-    width: 150px;
+  >main {
+    flex-grow: 1;
     padding: 16px;
-    position: fixed;
-    top: 0;
-    left: 0;
-    padding-top: 70px;
-    height: 100%;
-    > h2 {
-      margin-bottom: 4px;
-    }
-    > ol {
-      > li {
-        padding: 4px 0;
+    background: white;
+  }
+}
+aside {
+  background: lightblue;
+  width: 150px;
+  padding: 16px 0;
+  position: fixed;
+  top: 0;
+  left: 0;
+  padding-top: 70px;
+  height: 100%;
+  >h2 {
+    margin-bottom: 4px;
+    padding: 0 16px;
+  }
+  >ol {
+    >li {
+      >a {
+        display: block;
+        padding: 4px 16px;
+        text-decoration: none;
+      }
+      .router-link-active {
+        background: white;
       }
     }
   }
-  main {
-    overflow: auto;
-  }
+}
+main {
+  overflow: auto;
+}
 </style>
